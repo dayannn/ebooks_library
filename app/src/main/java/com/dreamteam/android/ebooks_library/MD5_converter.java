2854ee303c5d05d5432685cee0035ebb8c4d7aa2
Dayan <dayannn@yandex.ru>
2018-04-25 01:29:16 +0300

    sending POST request while auth #EBOOKS-3 in progress

---
+++ app/src/main/java/com/dreamteam/android/ebooks_library/MD5_converter.java
@@ -0,0 +1,35 @@
+package com.dreamteam.android.ebooks_library;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by dayan on 25.04.2018.
+ */
+
+public class MD5_converter {
+    public static final String md5(final String s) {
+        final String MD5 = "MD5";
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest
+                    .getInstance(MD5);
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuilder hexString = new StringBuilder();
+            for (byte aMessageDigest : messageDigest) {
+                String h = Integer.toHexString(0xFF & aMessageDigest);
+                while (h.length() < 2)
+                    h = "0" + h;
+                hexString.append(h);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
